f7e5061dc5f5f7516993a5e1ee2a50a0a024e3c8
Antonio Estevam da Silva Filho(Jhonny) <47607852+Jhonny3x4@users.noreply.github.com>
2021-06-22 23:46:35 -0300

    criação de tela de autenticação

---
+++ src/pages/Home.tsx
@@ -0,0 +1,42 @@
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import { Button } from '../components/Button';
+import googleIconImg from '../assets/images/google-icon.svg';
+import '../styles/auth.scss';
+
+export function Home(){
+
+    return(
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+
+                <strong>Crie salas de Q&amp;A ao-vivo</strong>
+                <p>Tire as duvidas de sua audiência em tempo-real</p>
+            </aside>
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="Letmeask"/>
+                    <button className="create-rom">
+                        <img src={googleIconImg} alt="Logo do Google" />
+                        Crie sua sala com a conta do Google
+                    </button>
+
+                    <div className="separator"> Ou entre em uma sala </div>
+
+                    <form >
+                        <input 
+                            type="text" 
+                            placeholder="Digite o código da sala"
+                        />
+                        
+                        <Button type="submit">
+                            Entrar na sala
+                        </Button>
+
+                    </form>
+                </div>
+            </main>
+        </div>
+    );
+}
